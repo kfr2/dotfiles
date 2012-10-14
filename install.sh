828acf6750ff0cc03ebb1e503aa5ed8a972217c6
Kevin Richardson <kevin@magically.us>
2012-10-13 21:38:41 -0400

    Remove leading periods from filenames

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Idea taken from https://github.com/myusuf3/dotfiles/blob/master/install.sh.
 # Thanks!
-ln -s .vimrc ~/.vimrc
-ln -s .bash_profile ~/.bash_profile
-ln -s .zshrc ~/.zshrc
-ln -s .crontab ~/.crontab
-ln -s .hushlogin ~/.hushlogin
-ln -s .hgrc ~/.hgrc
-ln -s .gitconfig ~/.gitconfig
-ln -s .gitignore ~/.gitignore
+ln -s vimrc ~/.vimrc
+ln -s zshrc ~/.zshrc
+ln -s tmux.conf ~/.tmux.conf
+ln -s crontab ~/.crontab
+ln -s hushlogin ~/.hushlogin
+ln -s hgrc ~/.hgrc
+ln -s gitconfig ~/.gitconfig
+ln -s gitignore ~/.gitignore
