d2edb651f741d45812d0b45d74b0585a768821f6
Kevin Richardson <kevin@magically.us>
2012-11-17 21:06:22 -0500

    Fix ln -s syntax in install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Idea taken from https://github.com/myusuf3/dotfiles/blob/master/install.sh.
 # Thanks!
-ln -s vimrc ~/.vimrc
-ln -s zshrc ~/.zshrc
-ln -s tmux.conf ~/.tmux.conf
-ln -s crontab ~/.crontab
-ln -s hushlogin ~/.hushlogin
-ln -s hgrc ~/.hgrc
-ln -s gitconfig ~/.gitconfig
-ln -s gitignore ~/.gitignore
+ln -s ~/dotfiles/vimrc ~/.vimrc
+ln -s ~/dotfiles/zshrc ~/.zshrc
+ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/crontab ~/.crontab
+ln -s ~/dotfiles/hushlogin ~/.hushlogin
+ln -s ~/dotfiles/hgrc ~/.hgrc
+ln -s ~/dotfiles/gitconfig ~/.gitconfig
+ln -s ~/dotfiles/gitignore ~/.gitignore
